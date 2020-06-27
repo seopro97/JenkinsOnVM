b4c6e52bbbed7afa5ab616b703b3ba609eb2bdb1
seopro97 <67213394+seopro97@users.noreply.github.com>
2020-06-27 15:53:57 +0900

    2
    2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I love YoungOk!');
+    res.send('I love YoungOk!!!');
 });
 
 app.get('/start', function(req, res){
